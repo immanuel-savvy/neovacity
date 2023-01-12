dbdb054754c92732ec3736b1d11239b6c16440eb
“immanuel-savvy” <immanuelsavvy@gmail.com>
2023-01-12 15:30:12 +0100

    1673533812123384482

--- src/Components/daily_outline.js
+++ src/Components/daily_outline.js
@@ -8,17 +8,22 @@ class Daily_outline extends React.Component {
   }
 
   render() {
+    let { outline, index } = this.props;
+    let { topic, resource } = outline;
+
     return (
       <div class="liop_wraps_single">
         <div class="lki_813">
           <h6>Day</h6>
-          <span>01</span>
+          <span>{`${index}`.padStart(2, "0")}</span>
         </div>
         <div class="bhu_486">
-          <h5>Introduction to Indian Administration, Kautilya's Arthshastra</h5>
-          <a>
-            <span>Download Resource</span>
-          </a>
+          <h5>{topic}</h5>
+          {resource ? (
+            <a>
+              <span>Download Resource</span>
+            </a>
+          ) : null}
         </div>
       </div>
     );
--- src/Components/forms/update_curriculum.js
+++ src/Components/forms/update_curriculum.js
@@ -1,23 +1,225 @@
 import React from "react";
-import { get_request } from "../../Assets/js/utils/services";
+import { post_request } from "../../Assets/js/utils/services";
+import { emitter } from "../../Neovacity";
+import Daily_outline from "../daily_outline";
+import Loadindicator from "../loadindicator";
 
 class Update_curriculum extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      weeks: new Array(),
+    };
   }
 
   componentDidMount = async () => {
     let { course } = this.props;
+    let weeks = await post_request(`curriculum/${course._id}`),
+      active_week;
 
-    let curriculum = await get_request(`curriculum/${course._id}`);
-    this.setState({ curriculum });
+    if (weeks && !weeks.length) {
+      weeks = new Array({
+        _id: Math.random(),
+        lectures: new Array(),
+        topic: "",
+      });
+    }
+    active_week = weeks[0]._id;
+
+    this.setState({ weeks, active_week });
   };
 
+  reset_state = () =>
+    this.setState({
+      topic: "",
+      subtopics: new Array(),
+      _id: null,
+    });
+
+  add_new_week = (e) => {
+    e.preventDefault();
+
+    let { weeks } = this.state;
+    weeks.push({
+      week: weeks.length + 1,
+      _id: Math.random(),
+      lectures: new Array(),
+    });
+
+    this.setState({ weeks });
+  };
+
+  handle_file = ({ target }) => {
+    console.log("here tho");
+    let file = target.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    this.setState({ cv_loading: true });
+    reader.onloadend = async (e) =>
+      this.setState({
+        resource: reader.result,
+        resource_file_name: file.name,
+        cv_loading: false,
+      });
+  };
+
+  delete_week = async (week_id) => {
+    let { course } = this.props;
+    let { weeks, active_week } = this.state;
+    weeks = weeks.filter((week) => week._id !== week_id);
+
+    if (active_week === week_id) active_week = weeks[0];
+    this.setState({ weeks, active_week });
+
+    await post_request("remove_week", { week: week_id, course: course._id });
+  };
+
+  save_lecture = () => {
+    let { active_week, topic, resource_file_name, resource, weeks } =
+      this.state;
+    let week = weeks.find((week) => week._id === active_week);
+    week.lectures.push({
+      topic,
+      resource,
+      resource_file_name,
+    });
+
+    this.setState({ weeks, resource_file_name: "", resource: "", topic: "" });
+  };
+
+  active_week = (week_id) => this.setState({ active_week: week_id });
+
   render() {
-    let { curriculum } = this.state;
-    return;
+    let { toggle } = this.props;
+    let { topic, weeks, active_week, resource_loading, resource_file_name } =
+      this.state;
+
+    if (active_week)
+      active_week = weeks.find((week) => week._id === active_week);
+
+    console.log(active_week && active_week.lectures.length, "HHHHAAAHHHAAAA");
+
+    return (
+      <div>
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <br />
+            <div className="modal-body">
+              <div style={{ display: "flex", flexDirection: "row" }}>
+                {weeks.map((week, index) => (
+                  <span>
+                    <a
+                      href="#"
+                      style={{
+                        marginHorizontal: 2,
+                        padding: 5,
+                        borderRadius: 10,
+                        borderWidth: 1,
+                        borderColor: "#333",
+                      }}
+                      onClick={() => this.active_week(week._id)}
+                      key={index}
+                    >{`Week ${index + 1}`}</a>
+
+                    <a
+                      onClick={() =>
+                        window.confirm(`Delete week ${index + 1}?`) &&
+                        this.delete_week(week._id)
+                      }
+                      className="btn btn-action"
+                    >
+                      <i className={`fas fa-window-close`}></i>
+                    </a>
+                  </span>
+                ))}
+
+                <a href="#" onClick={this.add_new_week}>
+                  <span>
+                    <i class="fa fa-plus"></i>
+                    {`New week`}
+                  </span>
+                </a>
+              </div>
+
+              <br />
+              <h6>Add Lecture</h6>
+              <form className="forms_block">
+                <div className="form-group smalls">
+                  <label>Topic*</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    onChange={({ target }) =>
+                      this.setState({ topic: target.value })
+                    }
+                    value={topic}
+                  />
+                </div>
+
+                <div className="form-group smalls">
+                  <label>Lecture Resource (PDF)</label>
+                  <div className="custom-file">
+                    <input
+                      type="file"
+                      className="custom-file-input"
+                      id="customFile"
+                      accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                      onChange={this.handle_file}
+                    />
+                    {resource_loading ? (
+                      <Loadindicator />
+                    ) : (
+                      <label className="custom-file-label" for="customFile">
+                        {resource_file_name || "Choose Resource"}
+                      </label>
+                    )}
+                  </div>
+                </div>
+
+                {topic ? (
+                  <a
+                    href="#"
+                    style={{ color: "#fff" }}
+                    onClick={this.save_lecture}
+                    class="btn theme-bg btn-md"
+                  >
+                    Save
+                  </a>
+                ) : null}
+              </form>
+
+              <hr />
+
+              {active_week && active_week.lectures.length > 0 ? (
+                <div className="form-group smalls">
+                  <button
+                    onClick={this.sumbit}
+                    type="button"
+                    className={`btn theme-bg full-width text-white`}
+                  >
+                    {typeof active_week._id === "string"
+                      ? "Update Week"
+                      : "Add Week"}
+                  </button>
+                </div>
+              ) : null}
+
+              {active_week
+                ? active_week.lectures.map((lecture, index) => (
+                    <Daily_outline
+                      outline={lecture}
+                      key={index}
+                      index={index}
+                    />
+                  ))
+                : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
